babb40f9010d37a85ceb60798f45e6177e236232
Hai Nguyen <haitnguyn@gmail.com>
2018-07-14 21:15:24 -0700

    Restructured code, added Material UI

--- src/main/java/com/drafttool/DatabaseLoader.java
+++ src/main/java/com/drafttool/DatabaseLoader.java
@@ -1,5 +1,7 @@
 package com.drafttool.crispyoctospork;
 
+import com.drafttool.domain.Player;
+import com.drafttool.domain.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
