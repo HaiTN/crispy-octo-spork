abc4295671fafb0e9d26f213f841729900e5c644
Hai Nguyen <haitnguyn@gmail.com>
2018-07-14 17:02:41 -0700

    Jk

---
+++ src/main/java/com/drafttool/crispyoctospork/DraftToolApplication.java
@@ -0,0 +1,12 @@
+package com.drafttool.crispyoctospork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DraftToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DraftToolApplication.class, args);
+	}
+}
---
+++ src/test/java/com/drafttool/crispyoctospork/DraftToolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.drafttool.crispyoctospork;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DraftToolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
